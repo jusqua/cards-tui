e9490d25d772dda546c57fcbcfbe1c6b3454034f
Ádrian Gama <adrian.gama.se@gmail.com>
2022-09-17 19:06:44 -0300

    feat: input reboot

--- main.c
+++ main.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include <float.h>
-#include <limits.h>
-#include <errno.h>
-#include <math.h>
 
 #ifdef _WIN32
 #define PAUSE "pause"
@@ -17,135 +11,82 @@
 #define CLEAR "clear"
 #endif
 
-char *get_string(const char *);
-int get_int(const char *);
-char get_char(const char *);
-float get_float(const char *);
+char *input(const char *);
 
-static void teardown(void);
-static size_t allocations = 0;
-static char **strings = NULL;
+static char **allocatedStrings = NULL;
+static size_t allocatedStringsLength = 0;
+static void teardownAllocations(void);
 
 int main(void) {
-  atexit(teardown);
+  atexit(teardownAllocations);
 
-  puts("Hello, World!");
   return 0;
 }
 
-// based on libcs50: https://github.com/cs50/libcs50
-char *get_string(const char *format) {
-  int c;
-  char *string, *buffer = NULL;
-  size_t size = 0;
-
-  if (allocations == SIZE_MAX / sizeof(char *))
+// simplified libcs50 get_string: https://github.com/cs50/libcs50
+char *input(const char *format) {
+  if (allocatedStringsLength == SIZE_MAX / sizeof (char *))
     return NULL;
+
+  int ch;
+  char *string = NULL;
+  size_t stringLength = 0;
 
   printf("%s", format);
 
-  while ((c = fgetc(stdin)) != '\r' && c != '\n' && c != EOF) {
-    if (size >= SIZE_MAX) {
-      free(buffer);
+  while ((ch = fgetc(stdin)) != '\r' && ch != '\n' && ch != EOF) {
+    if (stringLength == SIZE_MAX) {
+      free(string);
       return NULL;
     }
 
-    buffer = realloc(buffer, size + 1);
-    if (buffer == NULL) {
-      free(buffer);
+    char *tmpString = realloc(string, stringLength + 1);
+    if (tmpString == NULL) {
+      free(string);
       return NULL;
     }
-  buffer[size++] = c;
+    string = tmpString;
+
+    string[stringLength++] = ch;
+    tmpString = NULL;
   }
 
-  if (size == 0 && c == EOF) {
+  if (stringLength == 0 && ch == EOF)
     return NULL;
-  }
 
-  if (size == SIZE_MAX) {
-    free(buffer);
-    return NULL;
-  }
-
-  if (c == '\r' && (c = fgetc(stdin)) == '\n') {
-    if (c != EOF && ungetc(c, stdin) == EOF) {
-      free(buffer);
+  if (ch == '\r' && (ch = fgetc(stdin)) != '\n') {
+    if (ch != EOF && ungetc(ch, stdin) == EOF) {
+      free(string);
       return NULL;
     }
   }
 
-  string = realloc(buffer, size + 1);
-  if (string == NULL) {
+  char *tmpString = realloc(string, stringLength + 1);
+  if (tmpString == NULL) {
     free(string);
     return NULL;
   }
-  string[size] = '\0';
+  string = tmpString;
 
-  strings = realloc(strings, sizeof(char *) * (allocations + 1));
-  if (strings == NULL) {
+  string[stringLength] = '\0';
+
+  char **tmpAllocatedStrings = realloc(allocatedStrings, sizeof (char *) * (allocatedStringsLength + 1));
+  if (tmpAllocatedStrings == NULL) {
     free(string);
     return NULL;
   }
+  allocatedStrings = tmpAllocatedStrings;
 
-  strings[allocations++] = string;
+  allocatedStrings[allocatedStringsLength++] = string;
 
   return string;
 }
 
-int get_int(const char *format) {
-  char *line, *tail;
-  long num;
-  errno = 0;
-
-  while (true) {
-    line = get_string(format);
-    if (line == NULL)
-      return INT_MAX;
-
-    if (*line && !isspace((unsigned char) *line)) {
-      num = strtol(line, &tail, 10);
-      if (errno == 0 && !(*tail) && num >= INT_MIN && num < INT_MAX)
-        return num;
-    }
+static void teardownAllocations(void) {
+  if (allocatedStrings != NULL) {
+    for (size_t c = 0; c < allocatedStringsLength; c++)
+      free(allocatedStrings[c]);
+    free(allocatedStrings);
   }
 }
 
-char get_char(const char *format) {
-  char *line, c, d;
-
-  while (true) {
-    line = get_string(format);
-    if (line == NULL)
-      return CHAR_MAX;
-
-    if (sscanf(line, "%c%c", &c, &d) <= 1)
-      return c;
-  }
-}
-
-float get_float(const char *format) {
-  char *line, *tail;
-  float num;
-  errno = 0;
-
-  while (true) {
-    line = get_string(format);
-    if (line == NULL)
-      return FLT_MAX;
-
-    if (*line && !isspace((unsigned char) *line)) {
-      num = strtof(line, &tail);
-      if (errno == 0 && !(*tail) && isfinite(num) && num < FLT_MAX)
-        if (strcspn(line, "XxEePp") == strlen(line))
-          return num;
-    }
-  }
-}
-
-static void teardown(void) {
-  if (strings != NULL) {
-    for (size_t i = 0; i < allocations; i++)
-      free(strings[i]);
-    free(strings);
-  }
-}
